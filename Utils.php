0f0cd77d71fc203f515f62b9e398a793abc6c257
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-15 17:13:18 +0100

    [Utils] Fonctions de bases de la class

--- Utils.php
+++ Utils.php
@@ -1,4 +1,18 @@
 <?php
+// Contient divers utilitaires pour l'ensemble de la couche métier
 class Utils{
+     //Retourne un hachage de pass.
+    public function HashPassword(string $pass){
+        return string;
+    }
 
+    // Indique si le mot de passe respecte les consignes de sécurité.
+    public function IsPasswordSafe(string $pass){
+        return boolean;
+    }
+
+    // Indique le nombre de travail par jour pour une personne.
+    public function GetWorkHoursByDay(){
+        return int;
+    }
 }
