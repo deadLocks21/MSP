99ec71716b0e2a2025b6d65d4859ded2a4b4bb3b
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-15 17:20:03 +0100

    [Project] Getter et Setter de la class

--- Project.php
+++ Project.php
@@ -2,4 +2,23 @@
 class Project{
     private $ID;
     private $Name;
+
+    // Getter de la class Project
+    public function getID(){
+        return $this->ID;
+    }
+
+    public function getName(){
+        return $this->Name;
+    }
+
+
+    // Setter de la class Project
+    private function setID(int $aID){
+        $this->ID = $aID;
+    }
+
+    public function setName(string $aName){
+        $this->Name = $aName;
+    }
 }
