9e54b4eabba1c7244110c32d8f71568bd17577f4
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-15 17:02:59 +0100

    [User] Getter et Setter

--- User.php
+++ User.php
@@ -4,4 +4,40 @@ class User{
     private $Name;
     private $Login;
     private $PasswordHash;
+
+    
+    // Getter de la class User
+    public function getID(){
+        return $this->ID;
+    }
+
+    public function getName(){
+        return $this->Name;
+    }
+
+    public function getLogin(){
+        return $this->Login;
+    }
+
+    public function getPasswordHash(){
+        return $this->PasswordHash;
+    }
+
+
+    // Setter de la class User
+    private function setID(int $aID){
+        $this->ID = $aID;
+    }
+
+    private function setName(int $aName){
+        $this->Name = $aName;
+    }
+
+    private function setLogin(int $aLogin){
+        $this->Login = $aLogin;
+    }
+
+    private function setPasswordHash(int $aPasswordHash){
+        $this->PasswordHash = $aPasswordHash;
+    }
 }
