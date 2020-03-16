d444e30c021d113ef562fa130cb7d71b935e478f
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-16 13:56:42 +0100

    [LogicError] Constructeur de la class

--- LogicError.php
+++ LogicError.php
@@ -1,5 +1,8 @@
 <?php
 // Exception générale pour la couche métier
 class LogicError extends Exception {
-    
+    public function __construct($msg, $code = 0)
+    {
+        parent::__construct($msg, $code);
+    }
 }
