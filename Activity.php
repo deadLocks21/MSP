b2a2e0a41e494539ff57339eb8a312a712e5046e
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-16 12:41:56 +0100

    [Activity] Ajout des attributs

--- Activity.php
+++ Activity.php
@@ -1,5 +1,15 @@
 <?php
 // Représente une activité
 class Activity extends ActivityState {
-    
+    private $ID;
+    private $state;
+    private $summary;
+    private $start;
+    private $end;
+    private $duration;
+    private $probaleEnd;
+    private $Name;
+    private $Kind;
+    private $Details;
+
 }
