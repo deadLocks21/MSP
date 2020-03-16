237de35ee9170cc52f672e5fc1a4edd566df9807
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-16 17:44:13 +0100

    [LogicError] Reformatation

--- LogicError.php
+++ LogicError.php
@@ -6,3 +6,16 @@ class LogicError extends Exception {
         parent::__construct($msg, $code);
     }
 }
+
+// Exception indiquant que l'état de l'activité ne peut devenir celui indiqué
+class BadActivityStateError extends LogicError{
+    private $activity;
+    private $desiredState;
+
+    // Constructeur de la class
+    public function __construct(Activity $a, $ds)
+    {
+        $msg = 'Une erreur à été levé par l\'activité '.$a->getName().' en assayant de passer son état à '.$ds.'.';
+        parent::__construct($msg, 0);
+    }
+}
