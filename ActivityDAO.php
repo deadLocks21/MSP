3bb9b16ce0482614488b931d0d367f5ef20d3020
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-16 17:23:34 +0100

    [ActivityDAO] Ajout des méthodes

--- ActivityDAO.php
+++ ActivityDAO.php
@@ -1,5 +1,11 @@
 <?php
 // Classe gérant la persistance en BDD des activités
 class ActivityDAO{
+    public function ReadActivities(Project $p, User $u){
+        return array();  // Array de Activity
+    }
 
+    public function Update(Activity $a){
+
+    }
 }
