c0ee98efe1ea974d930a93c1fcc6adb4f9fb332e
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-16 14:00:52 +0100

    [BadActivityStateError] Création des attributs de la class

--- BadActivityStateError.php
+++ BadActivityStateError.php
@@ -1,5 +1,6 @@
 <?php
 // Exception indiquant que l'état de l'activité ne peut devenir celui indiqué
 class BadActivityStateError extends LogicError{
-
+    private $activity;
+    private $desiredState;
 }
