f514b4a50d3f491c20f15c44e394a655de173fb8
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-16 17:36:32 +0100

    [DaoError] Reformatage des fichiers

--- DaoError.php
+++ DaoError.php
@@ -6,3 +6,11 @@ class DaoError extends Exception {
         parent::__construct($msg, $code);
     }
 }
+
+// Erreur dans la connexion avec la base de données
+class DataBaseError extends DaoError {
+    // Constructeur de la class
+    public function __construct(){
+        parent::__construct('Erreur lors de la connexion à la DB', 0);
+    }
+}
