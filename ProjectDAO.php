4e7f3d876a22a5df9f8944537822b207e1eb6385
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-17 18:36:07 +0100

    [ProjectDAO] Possibilité de récupérer les projets d'un User

--- ProjectDAO.php
+++ ProjectDAO.php
@@ -1,7 +1,64 @@
 <?php
+require 'User.php';
+require 'Project.php';
+
 // Gère la base pour les objets Project
 class ProjectDAO{
+    private function connection(){
+        $conn = null;
+
+        try {
+            $conn = new PDO('mysql:host=localhost;dbname=dbMSP', 'DaoMSP', '!MdpDeMSP21.');
+        } catch (PDOException $e) {
+            throw new DataBaseError();
+        }
+
+        return $conn;
+    }
+
+    private function createProjects($numP){
+        $conn = $this->connection();
+
+        $res = array();
+        foreach ($numP as $num){
+            $req = $conn->query("SELECT nom FROM Projet WHERE ID=$num");
+            $name = ($req->fetchAll())[0]['nom'];
+            $req->closeCursor();
+
+            $p = new Project($num);
+            $p->setName($name);
+
+            $res[] = $p;
+        }
+
+        return $res;
+    }
+
     public function ReadProjects(User $user){
-        return array(); // Array de Projects
+        $login = $user->getLogin();
+        $requeteListProj = "SELECT projetID FROM Compose JOIN Projet ON projetID = Projet.ID JOIN Affecté ON Affecté.activitéID = Compose.activitéID JOIN Technicien ON Technicien.ID = technicienID JOIN Utilisateur ON Utilisateur.ID = Utilisateur_ID WHERE login='$login';";
+        $conn = $this->connection();
+
+        $req = $conn->query($requeteListProj);
+        $listProj = ($req->fetchAll());
+        $req->closeCursor();
+
+        $numProjects = array();
+        foreach ($listProj as $projet){
+            $numProjects[] = $projet['projetID'];
+        }
+
+        return $this->createProjects($numProjects);
     }
 }
+
+//
+//$u = new User(1);
+//$u->setName('Martin');
+//$u->setLogin('pmartin');
+//$u->setPasswordHash('746F746F');
+//
+//
+//$pdao = new ProjectDAO();
+//$p = $pdao->ReadProjects($u);
+//echo print_r($p);
