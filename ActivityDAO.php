9e58e2d17a7337f1035e38c632b5588ae0cc92f0
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-03-23 14:28:46 +0100

    [ActivityDAO] Refonte de la fonction de lecture.

--- ActivityDAO.php
+++ ActivityDAO.php
@@ -2,48 +2,10 @@
 require 'Project.php';
 require 'Activity.php';
 require 'User.php';
+require 'ToolsDAO.php';
 
 // Classe gérant la persistance en BDD des activités
 class ActivityDAO{
-    private function connection(){
-        $conn = null;
-
-        try {
-            $conn = new PDO('mysql:host=localhost;dbname=dbMSP', 'DaoMSP', '!MdpDeMSP21.');
-        } catch (PDOException $e) {
-            throw new DataBaseError();
-        }
-
-        return $conn;
-    }
-
-    private function actsDuProj(Project $p){
-        $c = $this->connection();
-        $id = $p->getID();
-        $demandeInfoAct = "SELECT * FROM Compose JOIN Activité ON activitéID = ID WHERE projetID = $id;";
-
-        $resInfoActs = $c->query($demandeInfoAct);
-        $infosActs = $resInfoActs->fetchAll();
-
-        $resInfoActs->closeCursor();
-        $c = null;
-
-        return $infosActs;
-    }
-
-    private function devForAnAct($id){
-        $c = $this->connection();
-        $queryTech = "SELECT * FROM Affecté JOIN Technicien ON technicienID = Technicien.id WHERE activitéID = ".$id.";";
-
-        $resTech = $c->query($queryTech);
-        $tech = $resTech->fetchAll();
-
-        $resTech->closeCursor();
-        $c = null;
-
-        return $tech;
-    }
-
     private function getState($s){
         $res = null;
 
@@ -60,62 +22,24 @@ class ActivityDAO{
         return $res;
     }
 
-    private function getName($id){
-        $c = $this->connection();
-
-        $resNom = $c->query('SELECT * FROM Activité_type WHERE id='.$id.';');
-        $res = ($resNom->fetchAll())[0][1];
-
-        $resNom->closeCursor();
-        $c = null;
-
-        return $res;
-    }
-
-    private function createAct($a){
-        $ID = $a['activitéID'];
-        $state = $this->getState($a['statut']);
-        $summary = $a['résumé'];
-        $start = $a['dateDébut'];
-        $end = $a['dateFin'];
-        $duration = $a['duréePrévue'];
-        $Kind = $a['IDType'];
-        $Name = $this->getName($Kind);
-        $Details = $a['détail'];
-
-        $act = new Activity($ID, $Name, $summary, $state);
-        $act->setStart($start);
-        $act->setEnd($end);
-        $act->setDuration($duration);
-        $act->setKind($Kind);
-        $act->setDetails($Details);
-
-        return $act;
-    }
-
     public function ReadActivities(Project $p, User $u){
+        $tDAO = new ToolsDAO();
 
-        $activitys = $this->actsDuProj($p);
-        $devID = $u->getID();
+        $activities = $tDAO->query("CALL ReadActivities(".$p->getID().", ".$u->getID().");");
 
+        $lesAct = array();
+        foreach ($activities as $a){
+            $act = new Activity($a['ActivitéID'], $a['nom'], $a['résumé'], $this->getState($a['statut']));
+            $act->setStart($a['dateDébut']);
+            $act->setEnd($a['dateFin']);
+            $act->setDuration($a['duréePrévue']);
+            $act->setKind($a['IDType']);
+            $act->setDetails($a['détail']);
 
-        $actsConcernees = array();
-        foreach ($activitys as $act){
-            $developpers = $this->devForAnAct($act['activitéID']);
-
-            foreach ($developpers as $dev){
-                if($dev['UTILISATEUR_ID'] == $devID){
-                    $actsConcernees[] = $act;
-                }
-            }
+            $lesAct[] = $act;
         }
 
-        $res = array();
-        foreach ($actsConcernees as $actC){
-            $res[] = $this->createAct($actC);
-        }
-
-        return $res;  // Array de Activity
+        return $lesAct;
     }
 
     public function Update(Activity $a){
