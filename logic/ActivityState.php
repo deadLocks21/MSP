c6759a63129d16ffd1a163b3cda2575fd9a52af6
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-04-18 14:44:02 +0200

    [ActivityState] Commentaires

--- logic/ActivityState.php
+++ logic/ActivityState.php
@@ -1,7 +1,23 @@
 <?php
+
+/**
+ * Enumération permettant de stocker les états d'une activité.
+ */
 class ActivityState{
+    /**
+     * Activité prévue.
+     */
     const PLANNED='PLANNED';
+    /**
+     * Activité en cours.
+     */
     const ONGOING='ONGOING';
+    /**
+     * Activité terminée.
+     */
     const FINISHED='FINISHED';
+    /**
+     * Activité annulée.
+     */
     const CANCELED='CANCELED';
 }
