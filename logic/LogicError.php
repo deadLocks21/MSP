af06d167a677c75c1c2022bb6bb77df36538d0d1
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-04-18 14:44:23 +0200

    [LogicError] Commentaires

--- logic/LogicError.php
+++ logic/LogicError.php
@@ -1,5 +1,8 @@
 <?php
-// Exception générale pour la couche métier
+
+/**
+ * Classe contenant les exceptions générales pour la couche métier
+ */
 class LogicError extends Exception {
     public function __construct($msg, $code = 0)
     {
@@ -7,12 +10,26 @@ class LogicError extends Exception {
     }
 }
 
-// Exception indiquant que l'état de l'activité ne peut devenir celui indiqué
+/**
+ * Exception indiquant que l'état de l'activité ne peut devenir celui indiqué
+ */
 class BadActivityStateError extends LogicError{
-    private $activity;
-    private $desiredState;
+    /**
+     * @var Activity Activité posant problème.
+     */
+    // private $activity;
+    /**
+     * @var ActivityState State que désire atteindre l'utilisateur.
+     */
+    // private $desiredState;
 
-    // Constructeur de la class
+    /**
+     * BadActivityStateError constructor.
+     *
+     *
+     * @param Activity $a       Activité posant problème.
+     * @param $ds ActivityState State que l'utilisateur cherche à atteindre.
+     */
     public function __construct(Activity $a, $ds)
     {
         $msg = 'Une erreur à été levé par l\'activité '.$a->getName().' en assayant de passer son état à '.$ds.'.';
