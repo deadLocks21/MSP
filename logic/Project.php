27ee2a09769202bd564859ff4e08e559569d1d5a
deadLocks21 <45351098+deadLocks21@users.noreply.github.com>
2020-04-18 14:48:40 +0200

    [Project] Commentaires

--- logic/Project.php
+++ logic/Project.php
@@ -1,30 +1,68 @@
 <?php
+/**
+ * Classe représentant un projet.
+ */
 class Project{
+    /**
+     * @var int ID du projet
+     */
     private $ID;
+    /**
+     * @var string Nom du projet
+     */
     private $Name;
 
 
-    // Constructeur de la class Project
+    /**
+     * Project constructor.
+     *
+     *
+     * @param int $aID ID du projet.
+     */
     public function __construct(int $aID){
         $this->setID($aID);
     }
 
 
-    // Getter de la class Project
+    // ASSESSEURS DES ATTRIBUTS DE LA CLASS
+    /**
+     * Assesseur de l'attribut ID
+     *
+     *
+     * @return int ID du projet
+     */
     public function getID(){
         return $this->ID;
     }
 
+    /**
+     * Assesseur de l'attribut Name
+     *
+     *
+     * @return string Nom du projet.
+     */
     public function getName(){
         return $this->Name;
     }
 
 
-    // Setter de la class Project
+    // MUTATEURS DES ATTRIBUTS DE LA CLASS
+    /**
+     * Mutateur de l'attribut ID.
+     *
+     *
+     * @param int $aID ID du projet.
+     */
     private function setID(int $aID){
         $this->ID = $aID;
     }
 
+    /**
+     * Mutateur de l'attribut Name.
+     *
+     *
+     * @param string $aName Nom du projet.
+     */
     public function setName(string $aName){
         $this->Name = $aName;
     }
